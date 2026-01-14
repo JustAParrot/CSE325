e2b7cd93f95d4dd86132d4a4a3e5de2211e96196
George Quito <george-quitog@hotmail.com>
2026-01-14 16:59:06 -0700

    W01

---
+++ W01 LA/MyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+
+// Days until xmas
+var now = DateTime.Now;
+// Use next year if already past 25
+var nextChristmasYear = (now.Month > 12) || (now.Month == 12 && now.Day > 25) ? now.Year + 1 : now.Year;
+var nextChristmas = new DateTime(nextChristmasYear, 12, 25);
+var daysUntilChristmas = (nextChristmas.Date - DateTime.Today).Days;
+
+Console.WriteLine($"Days until next Christmas: {daysUntilChristmas}");
