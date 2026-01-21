4f01f32b797145f412d76616be46f235f83ee44c
George Quito <george-quitog@hotmail.com>
2026-01-21 16:04:06 -0700

    W03

--- W03/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ W03/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c466cdd128605fd4c5bf67cceeead1307742a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b324926e619cbf7ab3fedc65ca01dbb019a4b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
