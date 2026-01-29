f5f07904d230a4cf4c3aea4c1fafae6e611a8a83
George Quito <george-quitog@hotmail.com>
2026-01-28 18:51:51 -0800

    W03 pizza asgmt

---
+++ BPizza/BlazingPizza/Model/PizzaTopping.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazingPizza;
+
+public class PizzaTopping
+{
+    public int PizzaId { get; set; }
+    public Pizza Pizza { get; set; } = default!;
+
+    public int ToppingId { get; set; }
+    public Topping Topping { get; set; } = default!;
+}
---
+++ BPizza/BlazingPizza/PizzaStoreContext.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions<PizzaStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+    public DbSet<PizzaSpecial> Specials => Set<PizzaSpecial>();
+    public DbSet<Topping> Toppings => Set<Topping>();
+    public DbSet<PizzaTopping> PizzaToppings => Set<PizzaTopping>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<PizzaTopping>()
+            .HasKey(pt => new { pt.PizzaId, pt.ToppingId });
+
+        modelBuilder.Entity<PizzaTopping>()
+            .HasOne(pt => pt.Pizza)
+            .WithMany(p => p.Toppings);
+
+        modelBuilder.Entity<PizzaTopping>()
+            .HasOne(pt => pt.Topping)
+            .WithMany();
+    }
+}
